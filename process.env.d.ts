f056172bb31bdd98e3a389c3d167ed5957c7dae3
AbulAlaJobayar <jobayar59@gmail.com>
2025-05-08 18:00:41 +0600

    first commit

---
+++ process.env.d.ts
@@ -0,0 +1,12 @@
+declare namespace NodeJS {
+  export type ProcessEnv = {
+    NODE_ENV: string;
+    PORT: number;
+    BCRYPT_SALT_ROUND: number;
+    JWT_SECRET: string;
+    EXPIRES_IN: string;
+    REFRESH_SECRET: string;
+    REFRESH_EXPIRES_IN: string;
+    SALT_ROUND:string
+  };
+}
---
+++ src/server.ts
@@ -0,0 +1,28 @@
+import { Server } from 'http';
+import app from './app';
+import config from './config';
+
+
+
+async function bootstrap() {
+  const server: Server = app.listen(config.port, () => {
+    console.log(`Server is running on port ${config.port}`);
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error: unknown) => {
+    exitHandler();
+  };
+
+  process.on('uncaughtException', unexpectedErrorHandler);
+  process.on('unhandledRejection', unexpectedErrorHandler);
+}
+
+bootstrap();
